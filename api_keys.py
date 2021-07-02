4b031ef9c3c3fb09dbd58e78e30e1121780b9ef7
johnobogne <johnrosemonobogne@gmail.com>
2021-07-02 18:22:07 +0800

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,6 +1,6 @@
 def client_keys(request) :
     if request == 'client_id' :
-        return 'PLACE YOUR CLIENT_ID HERE'
+        return 'PLACE YOUR CLIENT ID HERE'
     elif request == 'client_secret' :
         return 'PLACE YOUR CLIENT SECRET HERE'
     else :
