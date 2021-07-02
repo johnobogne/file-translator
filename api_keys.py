b695143f0039bb8829f278b29fd60a5e6669126c
johnobogne <johnobognee@gmail.com>
2021-07-02 20:11:58 +0800

    fix merging errors

--- api_keys.py
+++ api_keys.py
@@ -1,17 +1,8 @@
 <<<<<<< HEAD
 def client_keys(request) :
     if request == 'client_id' :
-        return '582cRrpcbsRn8TQO0Iw8'
-    elif request == 'client_secret' :
-        return 'blEq2duIsQ'
-    else :
-        return None
-=======
-def client_keys(request) :
-    if request == 'client_id' :
         return 'PLACE YOUR CLIENT ID HERE'
     elif request == 'client_secret' :
         return 'PLACE YOUR CLIENT SECRET HERE'
     else :
         return None
->>>>>>> 9a6d365b086b23afd6c2d7f97c2dfa5eabeb3176
